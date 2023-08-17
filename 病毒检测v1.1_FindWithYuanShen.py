51601b322693b6a7981f7187015b616063af0fcc
Xzhao <112802685+Xzhao233@users.noreply.github.com>
2023-08-17 13:44:18 +0800

    原神，卸载😡👊😡👊😡👊

---
+++ 病毒检测v1.1_FindWithYuanShen.py
@@ -0,0 +1,63 @@
+﻿import os
+import platform
+
+def finddisk():
+    print("开始查找电脑所有盘符...")
+    a = []
+    result = os.popen('wmic logicaldisk get caption')
+    print("查询结果：")
+    # 返回的结果是一个<class 'os._wrap_close'>对象，需要读取后才能处理
+    willfind = False
+    context = result.read()
+    for line in context.splitlines():
+        if willfind == True :
+            if line != '':
+                print(line.replace(" ",""))
+                a.append(line.replace(" ",""))
+
+        willfind = True
+    result.close()
+    return a # 会返回电脑所有盘符的列表，如['C:', 'D:', 'E:']
+
+def winfindgenshin():
+    a = finddisk()
+    b = False
+    for i in a:
+        print('正在查找' + i + "...")
+        """
+        if b == True:
+            continue # 已经找到了就不用继续了
+        else:
+            willrun = "cd " + i + "\\ && dir yuanshen* /s /b"
+            result = os.popen(willrun)
+        """
+        willrun = "cd /d" + i + "\\ && dir yuanshen* /s /b"
+        result = os.popen(willrun) # 出于稳定性，我决定使用史山代码
+
+        # 如果在查找C，那么就是cd c:\ && dir mhy* /s /b
+        # 查找目录下和子目录所有以yuanshen开头的文件
+        context = result.read()
+        print(context)
+        # print(type(context))
+        if ("uan" in context) == True :
+            b = True
+            # print("b = True")
+        result.close()
+    if b == True:
+        print("你的电脑存在病毒软件原神！！！")
+    else:
+        print("恭喜，你的电脑不存在病毒软件原神！！！")
+
+import readchar
+
+if platform.system().lower() == 'windows':
+    print("系统为Windows，开始运行...")
+    winfindgenshin()
+    print("按任意键结束...")
+    readchar.readchar()
+elif platform.system().lower() == 'linux':
+    print("按任意键结束...")
+    readchar.readchar()
+else:
+    print("按任意键结束...")
+    readchar.readchar()
---
+++ 病毒检测v2_FindWithYuanShen.py
@@ -0,0 +1,76 @@
+﻿import os
+import platform
+import threading
+import readchar
+
+
+class GenshinSearchThread(threading.Thread):
+    def __init__(self, disk):
+        super().__init__()
+        self.disk = disk
+        self.result = False
+
+    def run(self):
+        if self.is_disk_online():
+            self.result = self.search_genshin_virus()
+        else:
+            print("磁盘 " + self.disk + " 处于脱机状态")
+
+    def is_disk_online(self):
+        try:
+            willrun = 'cd /d ' + self.disk
+            result = os.system(willrun)
+            return result == 0
+        except Exception as e:
+            return False
+
+    def search_genshin_virus(self):
+        try:
+            willrun = 'cd /d ' + self.disk + ' && dir /s /b yuanshen* '
+            result = os.system(willrun)
+            return result == 0
+        except Exception as e:
+            print("搜索 " + self.disk + " 时出现错误：" + str(e))
+            return False
+
+def finddisk():
+    print("开始查找电脑所有盘符...")
+    a = []
+    result = os.popen('wmic logicaldisk get caption')
+    print("查询结果：")
+    context = result.read()
+    for line in context.splitlines():
+        if line.strip() and len(line.strip()) == 2:
+            print(line.strip())
+            a.append(line.strip())
+    result.close()
+    return a
+
+def winfindgenshin():
+    disks = finddisk()
+    threads = []
+    for disk in disks:
+        thread = GenshinSearchThread(disk)
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    found_virus = any(thread.result for thread in threads)
+    if found_virus:
+        print("你的电脑存在病毒软件原神！！！")
+    else:
+        print("恭喜，你的电脑不存在病毒软件原神！！！")
+
+if platform.system().lower() == 'windows':
+    print("系统为Windows，开始运行...")
+    winfindgenshin()
+    print("按任意键结束...")
+    readchar.readchar()
+elif platform.system().lower() == 'linux':
+    print("按任意键结束...")
+    readchar.readchar()
+else:
+    print("按任意键结束...")
+    readchar.readchar()
